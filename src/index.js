5990cd01a808efa5178f3f17ef42583535ce0243
Mathieu Baurin <mathieubaurin@MacBook-Pro-de-Mathieu.local>
2022-01-23 00:45:08 +0100

    layout

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+import 'antd/dist/antd.css';
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root'));
